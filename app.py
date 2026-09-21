7f2f929ef46e90679419349a31387093cc4cd056
HEMAVARSHINI M A <hemavarshini2007@gmail.com>
2026-09-21 18:46:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ st.set_page_config(
 # LOAD DATA
 # --------------------------------------------------
 
-df = pd.read_csv("Palo Alto Networks.csv")
+df = pd.read_excel("Palo Alto Networks.xls")
 
 
 # --------------------------------------------------
